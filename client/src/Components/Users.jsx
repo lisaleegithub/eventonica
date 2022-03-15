62310494a65685809c5e1d91397f21a4fe08160b
Lisa Lee <lisawlee04@gmail.com>
2022-03-14 21:41:31 -0700

    Implement feature to add user and user info upon clicking the submit button.

--- client/src/Components/Users.jsx
+++ client/src/Components/Users.jsx
@@ -8,23 +8,52 @@ const dory = { name: 'Dory', email: 'dory@gmail.com', id: '3' };
 
 const Users = () => {
     const [users, setUsers] = useState([marlin, nemo, dory])
+    const [name, setName] = useState('')
+    const [id, setId] = useState('')
+    const [email, setEmail] = useState('')
+
+    const handleSubmit = (e) => {
+        console.log("hello");
+        e.preventDefault();
+        const newUser = { id: id, name: name, email: email };
+        setUsers([...users, newUser]);
+        setName('');
+        setId('');
+        setEmail('');
+    };
 
     return (
     <section className="user-management">
         <h2>User Management</h2>
 
         <ul id="users-list">
-            {users.map((user) => <li key={user.id}>{user.name} {user.email}</li>)}
+            {users.map((user) => <li key={user.id}>{user.name}: {user.email}</li>)}
         </ul>
 
         <div>
             <h3>Add User</h3>
-            <form id="add-user" action="#">
+            {/* onSubmit added */}
+            <form id="add-user" action="#" onSubmit={handleSubmit}>
                 <fieldset>
                     <label>Name</label>
-                    <input type="text" id="add-user-name" />
+                    <input type="text" id="add-user-name" 
+                    value={name}
+                    onChange={(e) => setName(e.target.value)}/>
                 </fieldset>
-                {/* Add more form fields here */}
+
+                <fieldset>
+                    <label>ID</label>
+                    <input type="number" id="add-user-id" 
+                    value={id}
+                    onChange={(e) => setId(e.target.value)}/>
+                </fieldset>
+                
+                <fieldset>
+                    <label>Email</label>
+                    <input type="email" id="add-user-email"
+                    value={email}
+                    onChange={(e) => setEmail(e.target.value)} />
+                </fieldset>
                 <input type="submit" value="Add" />
             </form>
         </div>
@@ -34,7 +63,7 @@ const Users = () => {
             <form id="delete-user" action="#">
                 <fieldset>
                     <label>User ID</label>
-                    <input type="text" id="delete-user-id" />
+                    <input type="number" id="delete-user-id" />
                 </fieldset>
                 <input type="submit" />
             </form>
