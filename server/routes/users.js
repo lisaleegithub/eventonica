933174a1a87e7af184187129a46556f61eaa3053
Lisa Lee <lisawlee04@gmail.com>
2022-03-16 15:52:22 -0700

    Return data in the Users endpoint.

--- server/routes/users.js
+++ server/routes/users.js
@@ -1,9 +1,21 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+let mockUsers = [
+  { id: 1, name: 'Marlin', email: 'marlin@gmail.com' },
+  { id: 2, name: 'Nemo', email: 'nemo@gmail.com' },
+  { id: 3, name: 'Dory', email: 'dory@gmail.com' }
+];
+
+// /* GET users listing. */
+// router.get('/', function(req, res, next) {
+//   res.send('respond with a resource');
+// });
+
+// server/routes/users.js`
+router.get('/', function (req, res, next) {
+  console.log(req.body, 'the body');
+  res.json({ users: mockUsers });
 });
 
 module.exports = router;
